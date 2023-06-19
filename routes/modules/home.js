774188231cd27bc1b4bdf89911021f50cf058bed
Yiching Hsu <ychsu7149@gmail.com>
2023-06-19 23:08:09 +0800

    feat: modify index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const recordModel = require('../../models/recordModel')
 
 router.get('/', (req, res) =>{
-    res.render('index')
+    recordModel.find()
+    .lean()
+    .then( records => res.render('index', { records }))
+    .catch(error => console.log(error))
+    
 })
 
 module.exports = router
