3a107af220a4c85efff2e8470a9611a5e9b53a70
yiching-hsu <ychsu7149@gmail.com>
2023-07-19 23:32:01 +0800

    add google login function

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 const bcrypt = require('bcryptjs')
 const FacebookStrategy = require('passport-facebook').Strategy
 
@@ -33,6 +34,33 @@ module.exports = app => {
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
         callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName']
+    }, (accessToken, refreshToken, profile, done) => {
+        // console.log(profile)
+
+        const { name, email } = profile._json
+        UserModel.findOne({ email })
+            .then(user => {
+                if (user) return done(null, user)
+
+                const randomPassword = Math.random().toString(36).slice(-8)
+                bcrypt.genSalt(10)
+                    .then(salt => bcrypt.hash(randomPassword, salt))
+                    .then(hash => UserModel.create({
+                        name,
+                        email,
+                        password: hash
+                    }))
+                    .then(user => done(null, user))
+                    .catch(err => done(err, false))
+            })
+    }))
+
+
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK,
         profileFields: ['email', 'displayName']
     }, (accessToken, refreshToken, profile, done) => {
         // console.log(profile)
