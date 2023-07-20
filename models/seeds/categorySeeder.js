fad93d68733ef4b3dd83eb97eacbaf8b5cd562fe
yiching-hsu <ychsu7149@gmail.com>
2023-07-20 23:55:25 +0800

    modify categorySeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -6,11 +6,25 @@ if (process.env.NODE_ENV !== 'production') {
 const CategoryModel = require('../categoryModel')
 const db = require('../../config/mongoose')
 
-db.once('open', () => {
-    CategoryModel.create(categories)
-        .then(() => {
-            db.close()
-        })
-    console.log('categorySeeder建立成功')
-    
+const categories = [
+    ['家居物業', 'fa-home'],
+    ['交通出行', 'fa-shuttle-van'],
+    ['休閒娛樂', 'fa-grin-beam'],
+    ['餐飲食品', 'fa-utensils'],
+    ['其他', 'fa-pen']
+].map(category => ({
+    name: category[0],
+    icon: `<i class="fas ${category[1]}"></i>`
+}))
+
+db.once('open', async () => {
+    try {
+        await CategoryModel.create(categories)
+        console.log(categories)
+        console.log('categorySeeder建立成功')
+    } catch (error) {
+        console.error('建立categorySeeder失敗:', error)
+    } finally {
+        db.close()
+    }
 })
